851bfb722d4da3ab96e65df6826ca4e3e5408199
arkpalmer <arkpalmer@gmail.com>
2021-01-10 12:19:41 -0500

    mostly operator friend functions but also other stuff

--- blob-ptr.h
+++ blob-ptr.h
@@ -7,6 +7,8 @@
 #include <memory>
 #include <string>
 
+#include "types.h"
+
 // StrBlobPtr: 474, 475, 566-569
 template <typename T>
 class Blob_ptr
@@ -14,12 +16,17 @@ class Blob_ptr
 public:
     Blob_ptr() : curr_(0) { }
     // note that curr_ is intialized to off the end
-    Blob_ptr(Blob<T>& blob, size_t sz) : wptr_(blob.data_), curr_(sz) { }
+    Blob_ptr(Blob<T>& blob, size_t sz = 0) : wptr_(blob.data_), curr_(sz) { }
 
-    T& deref() const;
+    T& operator*() const;
+    T* operator->() const;
 
-// HERE use operator++ and operator-- instead of incr
-    Blob_ptr& incr();
+    // prefix versions
+    Blob_ptr& operator++();
+    Blob_ptr& operator--();
+    // postfix versions
+    Blob_ptr  operator++(int);
+    Blob_ptr  operator--(int);
 
 private:
     std::shared_ptr<std::vector<T>> check(size_t i, const std::string& msg) const;
@@ -39,7 +46,7 @@ std::shared_ptr<std::vector<T>> Blob_ptr<T>::check(size_t i, const std::string& 
         throw std::runtime_error("unbound Blob_ptr/nullptr");
     }
 
-    if (i <= ret->size())
+    if (i >= ret->size())
     {
         std::stringstream ss;
         ss << msg << " size:" << ret->size();
@@ -51,27 +58,76 @@ std::shared_ptr<std::vector<T>> Blob_ptr<T>::check(size_t i, const std::string& 
 }
 
 template <typename T>
-T& Blob_ptr<T>::deref() const
+T& Blob_ptr<T>::operator*() const
 {
     std::stringstream ss;
     ss << "deref past end, curr_:" << curr_;
 
     auto ref = check(curr_, ss.str());
 
-    return (*ref)[curr_];
+    return (*ref)[curr_]; // (*ref) is the vector to which this object points
 }
 
 template <typename T>
-Blob_ptr<T>& Blob_ptr<T>::incr()
+T* Blob_ptr<T>::operator->() const
+{
+    return &this->operator*(); 
+}
+
+template <typename T>
+Blob_ptr<T>& Blob_ptr<T>::operator++()
 {
     std::stringstream ss;
     ss << "can't incr, already past end, curr_:" << curr_;
 
     auto ref = check(curr_, ss.str());
 
+    // this is prefix operator++, so inc first
     ++curr_;
 
     return *this; // ugh, had to look this up
+}
+
+template <typename T>
+Blob_ptr<T>& Blob_ptr<T>::operator--()
+{
+    if (curr_ == 0)
+    {
+        std::stringstream ss;
+        ss << "can't dec, already at begin" << curr_;
+
+        throw std::out_of_range(ss.str());
+    }
+
+    --curr_;
+
+    return *this;
+}
+
+// we can't return a ref since we need to increment curr_ after we have
+// the value to be returned (we can't return a local copy of a saved ref)
+// I had tried this before having to look at the book:
+// blob-ptr.h:107:10: warning: reference to local variable ‘ret’ returned [-Wreturn-local-addr]
+//     auto ret = *this; //nope
+//
+template <typename T>
+Blob_ptr<T> Blob_ptr<T>::operator++(int)
+{
+    auto ret = *this;
+
+    ++*this; // prefix ++ will do check
+
+    return ret;
+}
+
+template <typename T>
+Blob_ptr<T> Blob_ptr<T>::operator--(int)
+{
+    auto ret = *this;
+
+    --*this; // prefix -- will do check
+
+    return ret;
 }
 
 #endif
--- blob.h
+++ blob.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <sstream>
+#include <sstream>
 #include <vector>
 #include <memory> 
 #include <utility>
@@ -11,16 +12,28 @@
 // (but I need it since it's used in check, but wondering)
 #include <string>
 
+#include "types.h"
+
 // p659, also see StrBlob:
 // § 12.1.1 (p. 456) and updated in § 12.1.6 (p. 475) and in Chapters 13 and 14
 
 template <typename> class Blob_ptr;
+template <typename> class Blob; // for parameter declaration in operator== forward declaration
+template <typename T> bool operator==(const Blob<T>&, const Blob<T>&);
+template <typename T> bool operator!=(const Blob<T>&, const Blob<T>&);
+template <typename T> std::ostream& operator<<(std::ostream&, const Blob<T>&);
+template <typename T> void swap(Blob<T>&, Blob<T>&);
 
 template <typename T>
 class Blob
 {
 public:
-    friend class Blob_ptr<T>;
+    friend class Blob_ptr<T>; // Blob_ptr references data_
+    // TBD exactly why we need the first <T> here but not in the definition
+    friend bool operator==<T>(const Blob<T>&, const Blob<T>&);
+    friend bool operator!=<T>(const Blob<T>&, const Blob<T>&);
+    friend std::ostream& operator<< <T>(std::ostream&, const Blob<T>&);
+    friend void swap<T>(Blob<T>&, Blob<T>&);
 
     typedef T value_type;
     typedef typename std::vector<T>::size_type size_type;
@@ -29,8 +42,16 @@ public:
     Blob();
     Blob(std::initializer_list<T> ilist);
 
+    Blob(const Blob& rhs); // copy ctor
+    Blob<T>& operator=(const Blob<T>& rhs);
+
     size_t size() const { return data_->size(); }
     bool empty() const { return data_->empty(); }
+
+    Blob_ptr<T> begin(); // const? or is there a cbegin?
+    Blob_ptr<T> end(); // const? or is there a cbegin?
+    // HERE
+    // end() (and follow-up to cbegin question above)
 
     void push_back(const T& el) { data_->push_back(el); }
     // move version; see § 13.6.3 (p. 548)
@@ -62,6 +83,56 @@ Blob<T>::Blob(std::initializer_list<T> ilist)  :
     data_(std::make_shared<std::vector<T>>(ilist))
 {
     std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
+template <typename T>
+Blob<T>::Blob(const Blob& rhs)
+{
+    data_ = std::make_shared<std::vector<T>>(*rhs.data_);
+}
+
+template <typename T>
+void swap(Blob<T>& lhs, Blob<T>& rhs)
+{
+    using std::swap;
+
+    // TBD data_ isn't a raw pointer, so would need more than just swap
+}
+
+// maybe could use copy/swap? (but see TBD above)
+template <typename T>
+Blob<T>& Blob<T>::operator=(const Blob<T>& rhs)
+{
+    if (this == &rhs)
+    {
+        return *this;
+    }
+
+    // how does book do it, github uses make_shared here (I guess more efficient that way)
+    auto temp = Blob(rhs);
+    // std::cout << __PRETTY_FUNCTION__ << " temp:" << temp << std::endl;
+    // github didn't do this, does writing over the shared_ptr free the mem
+    data_ = nullptr;
+
+    data_ = temp.data_;
+
+    return *this;
+}
+
+template <typename T>
+Blob_ptr<T> Blob<T>::begin()
+{
+    auto bp = Blob_ptr<T>(*this);
+
+    return bp;
+}
+
+template <typename T>
+Blob_ptr<T> Blob<T>::end()
+{
+    auto bp = Blob_ptr<T>(*this, size());
+
+    return bp;
 }
 
 template <typename T>
@@ -98,6 +169,55 @@ T& Blob<T>::operator[](size_t i)
     check(i, ss.str());
 
     return (*data_)[i];
+}
+
+template <typename T> 
+bool operator==(const Blob<T>& lhs, const Blob<T>& rhs)
+{
+    if (lhs.data_ == nullptr || rhs.data_ == nullptr)
+    {
+        throw std::out_of_range("nullptr");
+    }
+
+    return *lhs.data_ == *rhs.data_;
+}
+
+template <typename T> 
+bool operator!=(const Blob<T>& lhs, const Blob<T>& rhs)
+{
+    return !(lhs == rhs);
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Blob<T>& blob)
+{
+    if (blob.data_ == nullptr)
+    {
+        throw std::out_of_range("nullptr");
+    }
+
+    std::stringstream ss;
+
+    for (const auto& i : *blob.data_)
+    {
+        ss << i << ", ";
+    }
+
+    auto s = ss.str();
+
+    if (s.empty())
+    {
+        os << "blob is empty";
+        return os;
+    }
+
+    // remove ", "
+    s.pop_back();
+    s.pop_back();
+    
+    os << s;
+
+    return os;
 }
 
 #endif
