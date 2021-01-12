bbfe9e94c0f3e6a196ae3664acb20ede1a76ceab
arkpalmer <arkpalmer@gmail.com>
2021-01-12 15:10:47 -0500

    updates to Vec

--- vec.h
+++ vec.h
@@ -3,14 +3,20 @@
 
 #include <iostream>
 #include <memory>
+#include <sstream>
 
 // modify so as not to use construct or destroy (deprecated after C++ 11)
+
+template <typename> class Vec; // needed for parameters in operator<< etc
+
+template <typename T> std::ostream& operator<<(std::ostream& os, Vec<T>& v);
 
 template <typename T>
 class Vec
 {
 public:
     Vec() : elements_(nullptr), first_free_(nullptr), cap_(nullptr) {}
+    Vec(const Vec& other);
 
     size_t size() const { return first_free_ - elements_; }
     size_t capacity() const { return cap_ - elements_; }
@@ -23,15 +29,17 @@ public:
 
     void push_back(const T& el);
 
+    friend std::ostream& operator<< <T>(std::ostream& os, Vec& v);
+
 private:
     // will ensure there is room to add at least one element to Vec
     // If there isn't room, call reallocate to get more space
     void check_n_alloc();
-
-    // allocate space and copy a range of elements
-    void alloc_n_copy(const T* first_el, const T* last_el);
     // reallocate Vec when it runs out of space
     void reallocate();
+
+    // allocate space and copy a range of elements
+    std::pair<T*, T*> alloc_n_copy(const T* first_el, const T* last_el);
 
     std::allocator<T> allocator_;
 
@@ -39,6 +47,16 @@ private:
     T* first_free_;  // just after the last actual element
     T* cap_;         // just past the end of allocated memory
 };
+
+template <typename T>
+Vec<T>::Vec(const Vec<T>& other)
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    auto res = alloc_n_copy(other.begin(), other.end());
+
+    elements_   = res.first;
+    first_free_ = cap_ = res.second;
+}
 
 template <typename T>
 void Vec<T>::check_n_alloc()
@@ -49,16 +67,27 @@ void Vec<T>::check_n_alloc()
     }
 }
 
-// return pair<T*,T*> not sure why yet?
+// returns a pair of pointer, one at the start of the new space, the 
+// second one past the last element copied into the new space
 template <typename T>
-void Vec<T>::alloc_n_copy(const T* first_el, const T* last_el)
+std::pair<T*, T*> Vec<T>::alloc_n_copy(const T* first_el, const T* last_el)
 {
-    size_t required = last_el - first_el + 1;
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
 
-    size_t available = capacity() - size();
+    size_t required = last_el - first_el;
 
-    std::cout << "req:" << required << " avail:" << available << std::endl;
+    std::cout << "req:" << required << std::endl;
 
+    auto data = allocator_.allocate(required);
+
+    for (int i=0; i<required; ++i)
+    {
+        std::cout << *(first_el + i) << std::endl;
+        *data++ = *(first_el + i);
+    }
+
+    // data has been advanced, first is the first element
+    return {data - required, data};
 }
 
 template <typename T>
@@ -96,12 +125,39 @@ void Vec<T>::reallocate()
 template <typename T>
 void Vec<T>::push_back(const T& el)
 {
-    reallocate();
     std::cout << "push_back" << std::endl;
 
-// this is actual version, check_n_alloc calls reallocate()
-// check_n_alloc();
+    check_n_alloc();
     *first_free_++ = el;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, Vec<T>& v)
+{
+    std::cout << "operator<<" << std::endl;
+
+    std::stringstream ss;
+
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        ss << *it << ", ";
+    }
+    
+    // TODO make this some utility for repeated use
+    auto s = ss.str();
+
+    if (s.empty())
+    {
+        os << "Vec is empty";
+    }
+    else
+    {
+        s.pop_back();
+        s.pop_back();
+        os << s;
+    }
+
+    return os;
 }
 
 #endif
